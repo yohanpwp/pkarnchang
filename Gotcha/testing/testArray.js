2bdea50a32d437a2a9c806546685ca3fec193169
yohanpwp <pirawit831@gmail.com>
2024-06-12 15:15:55 +0700

    basic training

--- Gotcha/testing/testArray.js
+++ Gotcha/testing/testArray.js
@@ -1,11 +1,11 @@
 
-var filter = function(arr, fn) {
+var filter = function(arr, fn, init) {
     var filteredArr = [];
     // for (let i = 0; i < arr.length; ++i) {
     //     arr[i] = fn(arr[i], i);
     // }
     // return arr; ****จะรีเทินผลลัพธ์ เป็นค่า true,false ต้องกำหนดตัวแปรรับผลลัพธ์
-    for (var i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         if (fn(arr[i], i)) {
             filteredArr.push(arr[i]);
         }
@@ -13,6 +13,12 @@ var filter = function(arr, fn) {
     return console.log(filteredArr);
 };
 
-filter([0,10,20,30], function greaterThan10(num) {
-    return num > 10;
+filter([0,10,20,30,40], function greaterThan5(num) {
+    return num > 5;
 });
+
+const array = [4,2,6,4,5]
+const sum = array.reduceRight((prev, cur) => {
+    return prev + cur;
+}) ;
+console.log(sum);
