4a6a168791e7f89043b402e8749a24a47add8bc5
yohanpwp <pirawit831@gmail.com>
2024-08-25 01:36:32 +0700

    Delete Gotcha directory

--- Gotcha/testing/query.js
+++
@@ -1,5 +0,0 @@
-let _energyLevel = 100 ;
-function energyLevel() {
-    if ((typeof(_energyLevel)) === 'number' ) { return 'My current energy level is ' + _energyLevel }
-  }
-console.log(energyLevel());
